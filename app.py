9a808953aa58140fbeea86aeccdc33f307e113ca
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2022-09-18 16:51:05 +0530

    fixed multi member form and form response.

--- app.py
+++ app.py
@@ -24,17 +24,22 @@ TeamName, No of members, 1st Name, 1st number, 1st email, 2nd name, 2nd number, 
 
 app = Flask(__name__)
 
-def check_if_email_exists(email_id:str) -> bool:
-    with open('registrations.csv','r') as csv_file_obj:
-        csv_reader = csv.reader(csv_file_obj, delimiter=',')
-        line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-            continue
-        else:
-            if email_id == row[4] or email_id == row[7] or email_id == row[10] or email_id == row[13]:
-                return False
+def unique_email(email_id:str) -> bool:
+    try:
+        with open('registrations.csv','r') as csv_file_obj:
+            csv_reader = csv.reader(csv_file_obj, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                    continue
+                else:
+                    if email_id == row[4] or email_id == row[8] or email_id == row[12] or email_id == row[16]:
+                        return False
+    except FileNotFoundError:
+        a = open('registrations.csv','x')
+        a.close()
+        return True
     return True
 
 
@@ -51,17 +56,16 @@ def check_if_exists_in_directory(file_or_folder_name:str,directory:str='') -> bo
 
 
 def write_to_csv(data: dict):
+    print(data)
     # TeamName, No of members, 1st Name, 1st number, 1st email, 2nd name, 2nd number, 2nd email, 3rd Name, 3rd Number, 3 email, 4th Name, 4th Number, 4th email.    
-    header = ['TeamName', 'NoOfMembers', 'TeamLeadName', 'TeamLeadPhoneNumber', 'TeamLeadEmail', 'TransactionID1', '2ndMemberName', '2ndMemberPhone', '2ndMemberEmail', 'TransactionID2', '3rdMemberName', '3rdMemberNumber', '3rdMemberEmail', 'TransactionID3', '4thMemberName', '4thMemberNumber', '4thMemberEmail', 'TransactionID4']
+    header = ['TeamName', 'NoOfMembers', 'TeamLeadName', 'TeamLeadPhoneNumber', 'TeamLeadEmail', 'TransactionID1', '2ndMemberName', '2ndMemberPhoneNumber', '2ndMemberEmail', 'TransactionID2', '3rdMemberName', '3rdMemberPhoneNumber', '3rdMemberEmail', 'TransactionID3', '4thMemberName', '4thMemberPhoneNumber', '4thMemberEmail', 'TransactionID4']
     
-    row = [data['TeamName'], data['NoOfMembers'], data['TeamLeadName'], data['TeamLeadPhoneNumber'], data['TeamLeadEmail'], data['TransactionID1'], data['2ndMemberName'], data['2ndMemberPhone'], data['2ndMemberEmail'], data['TransactionID2'], data['3rdMemberName'], data['3rdMemberNumber'], data['3rdMemberEmail'], data['TransactionID3'], data['4thMemberName'], data['4thMemberNumber'], data['4thMemberEmail'], data['TransactionID4']]
+    row = [data['TeamName'], data['NoOfMembers'], data['TeamLeadName'], data['TeamLeadPhoneNumber'], data['TeamLeadEmail'], data['TransactionID1'], data['2ndMemberName'], data['2ndMemberPhoneNumber'], data['2ndMemberEmail'], data['TransactionID2'], data['3rdMemberName'], data['3rdMemberPhoneNumber'], data['3rdMemberEmail'], data['TransactionID3'], data['4thMemberName'], data['4thMemberPhoneNumber'], data['4thMemberEmail'], data['TransactionID4']]
     with open('registrations.csv','a') as csv_file_obj:
         csv_write = csv.writer(csv_file_obj, delimiter=',',lineterminator='\n')
         if check_if_exists_in_directory('registrations.csv'):
             csv_write.writerow(row)
-            print("came to if")
         else:
-            print("came to else")
             with open('registrations.csv','x'):
                 pass
             csv_write.writerow(header)
@@ -74,12 +78,13 @@ def data():
     phone_no = ""
     data = {}
     if request.method == "POST":
-        data['TeamName'] = request.form['TeamName']
+        print(request.form, "hello")
+        data["TeamName"] = request.form['TeamName']
         data["NoOfMembers"] = request.form["NoOfMembers"]
-        data['TeamLeadName'] = request.form['TeamLeadName']
+        data["TeamLeadName"] = request.form['TeamLeadName']
         data["TeamLeadPhoneNumber"] = request.form["TeamLeadPhoneNumber"]
         data["TeamLeadEmail"] = request.form["TeamLeadEmail"]
-        data["TranscationID1"] = request.form["TranscationID1"]
+        data["TransactionID1"] = request.form["TransactionID1"]
 
         data["2ndMemberName"] = request.form["2ndMemberName"]
         data["2ndMemberPhoneNumber"] = request.form["2ndMemberPhoneNumber"]
@@ -95,14 +100,17 @@ def data():
         data["4thMemberPhoneNumber"] = request.form["4thMemberPhoneNumber"]
         data["4thMemberEmail"] = request.form["4thMemberEmail"]
         data["TransactionID4"] = request.form["TransactionID4"]
+        print("here123")
+        print(data)
         
         emails = (data["TeamLeadEmail"], data["2ndMemberEmail"], data["3rdMemberEmail"], data["4thMemberEmail"])
-        numbers = (data["TeamLeadPhoneNumber"], data["2ndMemberPhone"], data["3rdMemberPhoneNumber"], data["4thMemberPhoneNumber"])
+        numbers = (data["TeamLeadPhoneNumber"], data["2ndMemberPhoneNumber"], data["3rdMemberPhoneNumber"], data["4thMemberPhoneNumber"])
         
         for email in emails:
             if email:
-                if check_if_email_exists(email):
+                if not unique_email(email):
                     message = "One of your Email IDs is already registered for this event"
+                    print(message)
                     return render_template("index.html", message=message)
                     #  return jsonify({"error":"Email address already in use"}),400
         
@@ -111,16 +119,14 @@ def data():
                 if len(number) != 10:
                     print(len(data["Contact_no_team_lead"]))
                     phone_no = "Please enter a valid Indian phone number (10 Digits)"
+                    print(phone_no)
+
                     return render_template("index.html", phone_no=phone_no)
         
         # Pattern = re.compile("^.{3,32}#[0-9]{4}$")
         write_to_csv(data)
     
     return render_template("index.html")
-
-@app.route("/data2",methods=["POST","GET"])
-def data2():
-    return render_template("data2.html")
 
 if __name__ == "__main__":
     app.run()
--- static/js/forms.js
+++ static/js/forms.js
@@ -8,14 +8,32 @@ function numberSelected() {
     containerSelected(strUser);
 }
 
+input_ids = [
+    "3rdMemberName",
+    "3rdMemberNumber",
+    "3rdMemberEmail",
+    "TransactionID3",
+    "4thMemberName",
+    "4thMemberNumber",
+    "4thMemberEmail",
+    "TransactionID4",
+];
+
+
 // turn display form none=>block
 function containerSelected(num) {
+    console.log(num);
     for (var i = 0; i < num; i++) {
         memberContainer[i].style.display = "block";
     }
     for (var i = num; i < 3; i++) {
         memberContainer[i].style.display = "none";
     }
-}
+    
+    // Sets the required element to True for number of team members
+    for (let input_index = 0; input_index < (num - 2) * 4; input_index++) {
+        document.getElementById(input_ids[input_index]).required = true;
+    }
+    }
 e.onchange = numberSelected;
 numberSelected();
